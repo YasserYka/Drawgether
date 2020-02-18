b549bb70c69b4f3d02268dceff935754f0b4ec21
YasserYka <yasseryka@gmail.com>
2020-02-18 20:36:42 +0300

    simple canvas setup

--- src/main/java/canvas/websocket/share/Application.java
+++ src/main/java/canvas/websocket/share/Application.java
@@ -1,4 +1,4 @@
-package canvas.websocket.share.canvas.websocket.share;
+package canvas.websocket.share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("#board");
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.strokeStyle = '#FF0000';
+context.lineCap = 'round';
+context.lineJoin = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(event){
+    if(!isDrawing)
+        return;
+    console.log(event);
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+
+    [lastX, lastY]  = [event.offsetX, event.offsetY];
+
+    context.lineTo(lastX, lastY);
+    context.stroke();
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true;
+    
+    [lastX, lastY]  = [event.offsetX, event.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
