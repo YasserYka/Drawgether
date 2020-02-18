7b44f303bf3664f40ec2f107404b29ee3857d1b1
YasserYka <yasseryka@gmail.com>
2020-02-18 19:21:08 +0300

    set up

---
+++ src/test/java/canvas/websocket/share/canvas/websocket/share/ApplicationTests.java
@@ -0,0 +1,13 @@
+package canvas.websocket.share.canvas.websocket.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
